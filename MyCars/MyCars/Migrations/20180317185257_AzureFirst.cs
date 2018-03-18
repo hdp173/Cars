08a5965ab35efa1e092b65898ca9246c68f17d5c
EntornoJT <vkurylo417@gmail.com>
2018-03-18 12:24:30 +0100

    publishing project

---
+++ MyCars/MyCars/Migrations/20180317185257_AzureFirst.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MyCars.Migrations
+{
+    public partial class AzureFirst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
