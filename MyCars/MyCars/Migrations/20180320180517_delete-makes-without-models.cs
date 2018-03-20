30fdf5ee16a9785477efba7b6a476ee290d0f20c
EntornoJT <vkurylo417@gmail.com>
2018-03-20 19:09:59 +0100

    delete makes without models

---
+++ MyCars/MyCars/Migrations/20180320180517_delete-makes-without-models.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCars.Migrations
+{
+    public partial class deletemakeswithoutmodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from makes where id not in (select makeid from models)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
